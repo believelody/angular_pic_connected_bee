f8763a42c62b7a7387db7f00c48093f4ca773982
believelody <believelody@gmail.com>
2019-01-23 13:27:21 +0100

    testing gsm communication

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,8 @@
 exports.handler = (event, context, callback) => {
     let res = 'yes we can';
 
+    console.log(event);    
+
     return callback(null, {
         statusCode: 200,
         body: JSON.stringify(res)
