2739a59bdc8abd9b30a488f246f84446112200fb
believelody <believelody@gmail.com>
2019-01-25 13:34:32 +0100

    Modify test.js file

--- routes/test.js
+++ routes/test.js
@@ -3,11 +3,11 @@ const Test = require('../models/test');
 const router = express.Router();
 
 router.get('/test', (req, res) => {
-    console.log(req.query);
+    console.log(req);
 
     const valeur = new Test({ value: req.query.value });
-
-    valeur.save().then(val => res.json(val));
+    res.json(req);
+    // valeur.save().then(val => res.json(req));
 });
 
 router.post('/envoi', (req, res) => {
