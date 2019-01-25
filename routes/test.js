af4aac910be9e5094572145e6041fa0e9020b558
believelody <believelody@gmail.com>
2019-01-25 14:37:25 +0100

    Test communication raspberry - web server: stringify response

--- routes/test.js
+++ routes/test.js
@@ -3,11 +3,11 @@ const Test = require('../models/test');
 const router = express.Router();
 
 router.get('/test', (req, res) => {
-    console.log(req);
+    // console.log(req.query);
 
-    const valeur = new Test({ value: req.query.value });
-    res.json(req);
-    // valeur.save().then(val => res.json(req));
+    // const valeur = new Test({ value: req.query.value });
+    res.json({ msg: req.query.value });
+    // valeur.save().then(val => res.json({ msg: val.value }));
 });
 
 router.post('/envoi', (req, res) => {
@@ -18,6 +18,6 @@ router.post('/envoi', (req, res) => {
     valeur.save().then(val => res.json(val));
 });
 
-router.get('/', (req, res) => res.json({ msg: 'yes we can' }));
+router.get('/', (req, res) => res.json({ msg: JSON.stringify(req.query) }));
 
 module.exports = router;
